869d0d50ca1ffc04f4a8a0ef7e9f83fad0460d76
Rob Griffiths <rob@bytespider.eu>
2018-02-27 16:17:46 +0000

    added getStandardSearch

--- src/Provider/Twitter.php
+++ src/Provider/Twitter.php
@@ -13,6 +13,8 @@ class Twitter extends AbstractProvider
 {
     use BearerAuthorizationTrait;
 
+    const API_VERSION = 1.1;
+
     public function getBaseAuthorizationUrl()
     {
         return null;
@@ -47,5 +49,36 @@ class Twitter extends AbstractProvider
     protected function createResourceOwner(array $response, AccessToken $token)
     {
         return null;
+    }
+
+    public function getStandardSearch(AccessToken $token, $query)
+    {
+        $response = $this->fetchStandardSearch($token, $query);
+        return $response;
+    }
+
+    public function getStandardSearchUrl($query, $countPerPage = 25)
+    {
+        return sprintf('https://api.twitter.com/%s/search/tweets.json?', static::API_VERSION) . http_build_query([
+            'q'     => $query,
+            'count' => $countPerPage,
+        ]);
+    }
+
+    protected function fetchStandardSearch(AccessToken $token, $query)
+    {
+        $url = $this->getStandardSearchUrl($query);
+
+        $request = $this->getAuthenticatedRequest(self::METHOD_GET, $url, $token);
+
+        $response = $this->getParsedResponse($request);
+
+        if (false === is_array($response)) {
+            throw new UnexpectedValueException(
+                'Invalid response received from Authorization Server. Expected JSON.'
+            );
+        }
+
+        return $response;
     }
 }
