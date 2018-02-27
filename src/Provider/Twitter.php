1d5032b42ca6407ca6c3a6b0a868e90be1984ef4
Rob Griffiths <rob@bytespider.eu>
2018-02-27 10:27:36 +0000

    initial commit

---
+++ src/Provider/Twitter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace League\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Tool\BearerAuthorizationTrait
+
+class Twitter extends AbstractProvider
+{
+    use BearerAuthorizationTrait;
+
+    public function getBaseAuthorizationUrl()
+    {
+    }
+
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return 'https://api.twitter.com/oauth2/token';
+    }
+
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return '';
+    }
+
+    protected function getDefaultScopes()
+    {
+        return [];
+    }
+
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+
+    }
+
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+
+    }
+}
